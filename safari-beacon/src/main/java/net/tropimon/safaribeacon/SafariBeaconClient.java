fb8873f7e437cb63fea821a5fa35c298ad423857
mrsilverfog-hash <mrsilverfog@gmail.com>
2026-05-14 09:45:19 +0200

    Update SafariBeaconClient.java

--- safari-beacon/src/main/java/net/tropimon/safaribeacon/SafariBeaconClient.java
+++ safari-beacon/src/main/java/net/tropimon/safaribeacon/SafariBeaconClient.java
@@ -1,6 +1,7 @@
 package net.tropimon.safaribeacon;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.tropimon.safaribeacon.render.SafariBeaconRenderer;
 
@@ -8,7 +9,14 @@ public class SafariBeaconClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Enregistrer le renderer de faisceau après le rendu du monde
         WorldRenderEvents.LAST.register(SafariBeaconRenderer::onWorldRenderLast);
+
+        // Réinitialiser le cache à chaque connexion/déconnexion
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            SafariBeaconRenderer.resetCache();
+        });
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            SafariBeaconRenderer.resetCache();
+        });
     }
 }
